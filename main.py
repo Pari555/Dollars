46f6af4cde2a7512ba7df9f3ed9955ba47b22460
Debanshi123050 <123050@eesd.org>
2021-02-08 23:30:16 +0000

    added stuff

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ one = 0
 
 # total == 326
 if(total >= 100):
-  hundred = total / 100
+  hundred = total // 100
   # 326 - 3 * 100 = 326 -300 = 26
   total = total % 100
 
 if(total >= 50):
-  fifty = total / 50
+  fifty = total // 50
   total = total % 50
 
 if(total >= 20):
-  twenty = total / 20
+  twenty = total // 20
   total = total % 20
 
 if(total >= 10):
-  ten = total / 10
+  ten = total // 10
   total = total % 10
 
 if(total >= 5):
-  five = total / 5
+  five = total // 5
   total = total % 5
 
 if(total >= 1):
-  one = total / 1
+  one = total // 1
   total = total % 1
   
 print("hundreds: " + str(hundred))
